0357bc1539876185079729596571aa3d538de897
kevinzzzzz <jie736805161@qq.com>
2025-02-09 13:38:18 +0800

    feat: save

--- src/pages/home2/components/silverWing.vue
+++ src/pages/home2/components/silverWing.vue
@@ -54,8 +54,150 @@
       <p class="sp SilverWingComp_part2_giftPoint">
         我的积分：<span>{{ giftPoint }}</span>
       </p>
+      <div class="SilverWingComp_part2_content">
+        <ul class="SilverWingComp_part2_content_list1">
+          <li
+            v-for="(item, idx) in redeemGift.slice(0, 4)"
+            :key="idx"
+            class="SilverWingComp_part2_content_list1_item"
+          >
+            <div class="SilverWingComp_part2_content_list1_item_img">
+              <img :src="item.image" alt="" />
+              <p>{{ item.name }}</p>
+            </div>
+            <div class="SilverWingComp_part2_content_list1_item_btn sp">
+              {{ item.points }}积分兑换
+            </div>
+          </li>
+        </ul>
+        <ul class="SilverWingComp_part2_content_list2">
+          <li
+            v-for="(item, idx) in redeemGift.slice(4)"
+            :key="idx"
+            class="SilverWingComp_part2_content_list2_item"
+          >
+            <div class="SilverWingComp_part2_content_list2_item_img">
+              <img :src="item.image" alt="" />
+              <p>{{ item.name }}</p>
+            </div>
+            <div class="SilverWingComp_part2_content_list2_item_btn sp">
+              {{ item.points }}积分兑换
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
+    <div class="SilverWingComp_part3">
+      <h2 class="SilverWingComp_part3_title sp"></h2>
+      <div class="SilverWingComp_part3_content">
+        <div class="SilverWingComp_part3_content_block">
+          <div class="SilverWingComp_part3_content_block_left">
+            <h2 class="sp"></h2>
+            <p class="SilverWingComp_part3_content_block_left_giftPoint">
+              我的超导宝箱：<span>{{ superconductivityBox }}</span>
+            </p>
+            <ul class="SilverWingComp_part3_content_block_left_list">
+              <li
+                v-for="(item, idx) in superconductivityL"
+                :key="idx"
+                class="SilverWingComp_part3_content_block_left_list_item"
+              >
+                <div
+                  class="SilverWingComp_part3_content_block_left_list_item_img"
+                >
+                  <img :src="item.image" alt="" />
+                  <p>{{ item.name }}</p>
+                </div>
+                <div
+                  class="SilverWingComp_part3_content_block_left_list_item_btn sp"
+                >
+                  立即领取
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="SilverWingComp_part3_content_block_right">
+            <h2 class="sp"></h2>
+            <p class="SilverWingComp_part3_content_block_right_giftPoint">
+              我的超导值：<span>{{ superconductivityVal }}</span>
+            </p>
+            <ul class="SilverWingComp_part3_content_block_right_list">
+              <li
+                v-for="(item, idx) in superconductivityR"
+                :key="idx"
+                class="SilverWingComp_part3_content_block_right_list_item"
+              >
+                <div
+                  class="SilverWingComp_part3_content_block_right_list_item_img"
+                >
+                  <img :src="item.image" alt="" />
+                  <p>{{ item.name }}</p>
+                </div>
+                <div
+                  class="SilverWingComp_part3_content_block_right_list_item_btn sp"
+                >
+                  {{ item.points }}超导值兑换
+                </div>
+                <p>(单Q限兑换1次)</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="SilverWingComp_part3_content_block1">
+          <h2 class="sp"></h2>
+          <p>
+            注:如道具在私人金库中，请取出后再尝试领取<br />
+            当前绑定大区拥有以下道具，可领取对应超导值
+          </p>
 
+          <ul class="SilverWingComp_part3_content_block1_list">
+            <li
+              v-for="(item, idx) in superconductivityB"
+              :key="idx"
+              class="SilverWingComp_part3_content_block1_list_item"
+            >
+              <div class="SilverWingComp_part3_content_block1_list_item_img">
+                <img :src="item.image" alt="" />
+                <p>{{ item.name }}</p>
+              </div>
+              <div class="SilverWingComp_part3_content_block1_list_item_btn sp">
+                领取5超导值
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="SilverWingComp_part4">
+      <h2 class="SilverWingComp_part4_title sp"></h2>
+      <p class="sp SilverWingComp_part4_giftPoint">
+        我的2025万能碎片：<span>{{ universalShip }}</span>
+      </p>
+      <div class="SilverWingComp_part4_content">
+        <ul class="SilverWingComp_part4_content_list">
+          <li
+            v-for="(item, idx) in universalShipList"
+            :key="idx"
+            class="SilverWingComp_part4_content_list_item"
+          >
+            <div
+              class="SilverWingComp_part4_content_list_item_img"
+            >
+              <img :src="item.image" alt="" />
+              <p>{{ item.name }}</p>
+            </div>
+            <div
+              class="SilverWingComp_part4_content_list_item_btn sp"
+            >
+              {{ item.points }}碎片兑换
+            </div>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+    <div class="SilverWingComp_part5">
+    </div>
   </div>
 </template>
 
@@ -65,20 +207,26 @@ export default {
 };
 </script>
 <script setup lang="ts">
-import { ref } from "vue"
-import pic10026 from '@/assets/images/page2/10026.png'
-import pic10003 from '@/assets/images/page2/10003.png'
-import pic10004 from '@/assets/images/page2/10004.png'
-import pic10027 from '@/assets/images/page2/10027.png'
-import pic10008 from '@/assets/images/page2/10008.png'
-import pic10009 from '@/assets/images/page2/10009.png'
-import pic10010 from '@/assets/images/page2/10010.png'
-import pic100121 from '@/assets/images/page2/100121.png'
-import pic100122 from '@/assets/images/page2/100122.png'
-import pic10007 from '@/assets/images/page2/10007.png'
-import pic100082 from '@/assets/images/page2/100082.png'
-import pic100092 from '@/assets/images/page2/100092.png'
-
+import { ref } from "vue";
+import pic10026 from "@/assets/images/page2/10026.png";
+import pic10003 from "@/assets/images/page2/10003.png";
+import pic10004 from "@/assets/images/page2/10004.png";
+import pic10027 from "@/assets/images/page2/10027.png";
+import pic10008 from "@/assets/images/page2/10008.png";
+import pic10009 from "@/assets/images/page2/10009.png";
+import pic10010 from "@/assets/images/page2/10010.png";
+import pic100121 from "@/assets/images/page2/100121.png";
+import pic100112 from "@/assets/images/page2/100112.png";
+import pic10007 from "@/assets/images/page2/10007.png";
+import pic100082 from "@/assets/images/page2/100082.png";
+import pic100092 from "@/assets/images/page2/100092.png";
+import pic100371 from "@/assets/images/page2/100371.png";
+import pic100381 from "@/assets/images/page2/100381.png";
+import pic100391 from "@/assets/images/page2/100391.png";
+import pic10019 from "@/assets/images/page2/10019.png";
+import pic10047 from "@/assets/images/page2/10047.png";
+import pic10049 from "@/assets/images/page2/10049.png";
+import pic10051 from "@/assets/images/page2/10051.png";
 
 const purchaseOptions = ref([
   {
@@ -124,16 +272,253 @@ const btnOptions = ref([
     num: 0,
   },
 ]);
-const giftPoint = ref(0);
+const giftPoint = ref(0); // 我的积分
+const redeemGift = ref([
+  // 兑换版块
+  {
+    id: 1,
+    name: "QBZ191-银翼",
+    image: pic10026,
+    points: 1398,
+  },
+  {
+    id: 2,
+    name: "研",
+    image: pic10003,
+    points: 1168,
+  },
+  {
+    id: 3,
+    name: "月兔特工",
+    image: pic10004,
+    points: 888,
+  },
+  {
+    id: 4,
+    name: "金色蔷薇-量子蔷薇皮肤",
+    image: pic10027,
+    points: 698,
+  },
+  {
+    id: 5,
+    name: "M4A1-雷神音效卡",
+    image: pic10008,
+    points: 548,
+  },
+  {
+    id: 6,
+    name: "M4A1-雷神",
+    image: pic10009,
+    points: 298,
+  },
+  {
+    id: 7,
+    name: "AK47-无影",
+    image: pic10010,
+    points: 278,
+  },
+  {
+    id: 8,
+    name: "AA-12-雷霆",
+    image: pic100121,
+    points: 236,
+  },
+  {
+    id: 9,
+    name: "Barrett-暗月传说",
+    image: pic100112,
+    points: 198,
+  },
+  {
+    id: 10,
+    name: "白银末日机甲玩偶",
+    image: pic10007,
+    points: 108,
+  },
+  {
+    id: 11,
+    name: "交易专用钥匙x5",
+    image: pic100082,
+    points: 15,
+  },
+  {
+    id: 12,
+    name: "属性变更券x5",
+    image: pic100092,
+    points: 15,
+  },
+  {
+    id: 13,
+    name: "交易专用钥匙x1",
+    image: pic100082,
+    points: 3,
+  },
+  {
+    id: 14,
+    name: "属性变更券x1",
+    image: pic100092,
+    points: 3,
+  },
+]);
+const superconductivityL = ref([
+  {
+    id: 1,
+    name: "超导值x3",
+    image: pic100371,
+    points: 0,
+  },
+  {
+    id: 2,
+    name: "王者之石x1",
+    image: pic100381,
+    points: 0,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 0,
+  },
+]);
+const superconductivityR = ref([
+  {
+    id: 1,
+    name: "QBZ191-银翼",
+    image: pic100371,
+    points: 148,
+  },
+  {
+    id: 2,
+    name: "王者之石x1",
+    image: pic100381,
+    points: 118,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 88,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 48,
+  },
+]);
+const superconductivityBox = ref(0); // 超导宝箱
+const superconductivityVal = ref(0); // 超导值
+
+const superconductivityB = ref([
+  {
+    id: 1,
+    name: "QBZ191-银翼",
+    image: pic100371,
+    points: 0,
+  },
+  {
+    id: 2,
+    name: "王者之石x1",
+    image: pic100381,
+    points: 0,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 0,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 0,
+  },
+  {
+    id: 1,
+    name: "QBZ191-银翼",
+    image: pic100371,
+    points: 0,
+  },
+  {
+    id: 2,
+    name: "王者之石x1",
+    image: pic100381,
+    points: 0,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 0,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 0,
+  },
+  {
+    id: 1,
+    name: "QBZ191-银翼",
+    image: pic100371,
+    points: 0,
+  },
+  {
+    id: 2,
+    name: "王者之石x1",
+    image: pic100381,
+    points: 0,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 0,
+  },
+  {
+    id: 3,
+    name: "2025万能碎石x3",
+    image: pic100391,
+    points: 0,
+  },
+]);
+const universalShip = ref(0); // 2025万能碎片
+const universalShipList = ref([
+  {
+    id: 1,
+    name: "游戏内王者夺宝红宝石x1",
+    image: pic10019,
+    points: 10,
+  },
+  {
+    id: 2,
+    name: "游戏内王者轮回蓝宝石x1",
+    image: pic10047,
+    points: 10,
+  },
+  {
+    id: 3,
+    name: "游戏内王者许愿黄宝石x1",
+    image: pic10049,
+    points: 10,
+  },
+  {
+    id: 4,
+    name: "本活动抽奖钥匙x1",
+    image: pic10051,
+    points: 10,
+  },
+]);
 </script>
 
 <style scoped lang="less">
 .SilverWingComp {
   width: 100%;
   // height: 100%;
-  // background: url("@/assets/images/page2/10053.jpg") no-repeat;
+  background: url("@/assets/images/page2/10053.jpg") no-repeat;
   background-size: 100% 100%;
-  padding: 10px 0;
+  padding: 10px 0 50px;
   &_btnList {
     height: 40px;
     display: grid;
@@ -246,12 +631,13 @@ const giftPoint = ref(0);
   }
 
   &_part2 {
+    margin-top: 20px;
     width: 100%;
     padding: 0 5px;
     &_title {
       width: 100%;
       height: 36px;
-      background-position: 0rem -8rem !important;
+      background-position: 0rem -5.45rem !important;
     }
     &_giftPoint {
       width: 33%;
@@ -265,6 +651,293 @@ const giftPoint = ref(0);
         color: #f5750c;
       }
     }
+    &_content {
+      width: 100%;
+      &_list1,
+      &_list2 {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 0px;
+        &_item {
+          width: 100%;
+          &_img {
+            width: 100%;
+            height: 50px;
+            background: url("@/assets/images/page2/10046.png") no-repeat;
+            background-size: 100% 100%;
+            padding: 1px;
+            position: relative;
+            img {
+              width: 99%;
+              height: 99%;
+            }
+            p {
+              width: 100%;
+              font-size: 4px;
+              color: #2f5cb5;
+              text-align: center;
+              position: absolute;
+              left: 0%;
+              bottom: 6px;
+            }
+          }
+          &_btn {
+            width: 100%;
+            height: 25px;
+            background-position: 0.2rem -6.5rem !important;
+            font-size: 8px;
+            margin-top: 2px;
+            font-weight: 600;
+            color: #def5ff;
+            text-align: center;
+            line-height: 25px;
+          }
+        }
+      }
+      &_list2 {
+        margin-top: 5px;
+        grid-template-columns: repeat(5, 1fr);
+        &_item {
+          margin-bottom: 5px;
+          &_btn {
+            width: 100%;
+            height: 25px;
+            background-position: -0.1rem -6.5rem !important;
+            font-size: 8px;
+            margin-top: 2px;
+            font-weight: 600;
+            color: #def5ff;
+            text-align: center;
+            line-height: 25px;
+          }
+        }
+      }
+    }
+  }
+
+  &_part3 {
+    margin-top: 20px;
+    width: 100%;
+    padding: 0 5px;
+    &_title {
+      width: 100%;
+      height: 36px;
+      background-position: 0rem -8.2rem !important;
+    }
+    &_content {
+      width: 100%;
+      &_block {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 5px;
+        &_left,
+        &_right {
+          width: 100%;
+          background: url("@/assets/images/page2/10065.png") no-repeat;
+          background-size: 100% 100%;
+          padding: 10px;
+          h2 {
+            width: 60%;
+            margin: 0 auto;
+            height: 20px;
+            background-position: -2.1rem -7.35rem !important;
+          }
+          &_giftPoint {
+            width: 60%;
+            margin: 10px auto;
+            padding: 5px 10px;
+            font-size: 8px;
+            color: #fff;
+            text-align: center;
+            span {
+              color: #fffabf;
+            }
+          }
+          &_list {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-gap: 0px;
+            &_item {
+              width: 100%;
+              margin-bottom: 5px;
+              &_img {
+                width: 100%;
+                height: 50px;
+                background: url("@/assets/images/page2/10046.png") no-repeat;
+                background-size: 100% 100%;
+                padding: 1px;
+                position: relative;
+                img {
+                  width: 99%;
+                  height: 99%;
+                }
+                p {
+                  width: 100%;
+                  font-size: 4px;
+                  color: #2f5cb5;
+                  text-align: center;
+                  position: absolute;
+                  left: 0%;
+                  bottom: 6px;
+                }
+              }
+              &_btn {
+                width: 100%;
+                height: 25px;
+                background-position: 0rem -7.3rem !important;
+                font-size: 8px;
+                margin-top: 2px;
+                font-weight: 600;
+                color: #744004;
+                text-align: center;
+                line-height: 25px;
+              }
+              p {
+                font-size: 6px;
+                color: #fff;
+                text-align: center;
+              }
+            }
+          }
+        }
+        &_right {
+          h2 {
+            background-position: -4.5rem -7.35rem !important;
+          }
+        }
+      }
+      &_block1 {
+        margin-top: 10px;
+        width: 100%;
+        background: url("@/assets/images/page2/10066.png") no-repeat;
+        background-size: 100% 100%;
+        padding: 10px;
+        h2 {
+          width: 20%;
+          margin: 0 auto;
+          height: 20px;
+          background-position: -7.7rem -6.3rem !important;
+        }
+        p {
+          margin-top: 5px;
+          font-size: 6px;
+          color: #fff;
+          text-align: center;
+        }
+        &_list {
+          display: grid;
+          grid-template-columns: repeat(4, 1fr);
+          grid-gap: 0px;
+          &_item {
+            width: 100%;
+            &_img {
+              width: 100%;
+              height: 50px;
+              background: url("@/assets/images/page2/10046.png") no-repeat;
+              background-size: 100% 100%;
+              padding: 1px;
+              position: relative;
+              img {
+                width: 99%;
+                height: 99%;
+              }
+              p {
+                width: 100%;
+                font-size: 4px;
+                color: #2f5cb5;
+                text-align: center;
+                position: absolute;
+                left: 0%;
+                bottom: 6px;
+              }
+            }
+            &_btn {
+              width: 100%;
+              height: 25px;
+              background-position: 0.1rem -7.3rem !important;
+              font-size: 8px;
+              margin-top: 2px;
+              font-weight: 600;
+              color: #744004;
+              text-align: center;
+              line-height: 25px;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  &_part4 {
+    margin-top: 20px;
+    width: 100%;
+    padding: 0 5px;
+    &_title {
+      width: 100%;
+      height: 36px;
+      background-position: 0rem -9.2rem !important;
+    }
+    &_giftPoint {
+      width: 33%;
+      margin: 10px auto;
+      padding: 5px 10px;
+      background-position: -2rem -6.5rem !important;
+      font-size: 8px;
+      color: #2f5cb5;
+      text-align: center;
+      span {
+        color: #f5750c;
+      }
+    }
+    &_content {
+      &_list {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 0px;
+        &_item {
+          width: 100%;
+          &_img {
+            width: 100%;
+            height: 50px;
+            background: url("@/assets/images/page2/10046.png") no-repeat;
+            background-size: 100% 100%;
+            padding: 1px;
+            position: relative;
+            img {
+              width: 99%;
+              height: 99%;
+            }
+            p {
+              width: 100%;
+              font-size: 4px;
+              color: #2f5cb5;
+              text-align: center;
+              position: absolute;
+              left: 0%;
+              bottom: 6px;
+            }
+          }
+          &_btn {
+            width: 100%;
+            height: 25px;
+            background-position: 0.18rem -6.5rem !important;
+            font-size: 8px;
+            margin-top: 2px;
+            font-weight: 600;
+            color: #def5ff;
+            text-align: center;
+            line-height: 25px;
+          }
+        }
+      }
+    }
+  }
+  &_part5 {
+    margin: 20px auto 0;
+    width: 96%;
+    height: 300px;
+    background: url("@/assets/images/page2/10069.png") no-repeat;
+    background-size: 100% 100%;
   }
 }
 .sp {
